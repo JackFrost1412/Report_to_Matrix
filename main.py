5bd1b565a7b42714529ad78f5f5a34bd2de32f1f
Dai <daidb1504@gmail.com>
2024-10-11 10:22:08 +0700

    update config

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from Report_to_Matrix import report_to_matrix
+import configparser
 
-file_path = fr"demo.xlsx"
+# Đọc đường dẫn thư mục từ file config.ini
+config = configparser.ConfigParser()
+try:
+    config.read('config.ini', encoding='utf-8')
+    file = config['files']['file_name']
+except (FileNotFoundError, KeyError) as e:
+    print(f"Error reading config file: {e}")
+    exit(1)
+
+file_path = fr"Matrix_BRD\{file}"
 
 report_to_matrix(file_path)
